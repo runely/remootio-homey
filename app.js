a41a130f3175d681b0cace0c60371d5f44655c14
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2022-11-17 19:27:17 +0100

    Renamed app

--- app.js
+++ app.js
@@ -2,13 +2,13 @@
 
 const Homey = require('homey')
 
-class MyApp extends Homey.App {
+class Remootio extends Homey.App {
   /**
    * onInit is called when the app is initialized.
    */
   async onInit () {
-    this.log('MyApp has been initialized')
+    this.log('Remootio has been initialized')
   }
 }
 
-module.exports = MyApp
+module.exports = Remootio
